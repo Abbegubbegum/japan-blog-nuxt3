214429840b088c714ef04a15512191c4e13f4eed
Albin Norback <albin.nojback@gmail.com>
2023-01-28 19:13:06 +0900

    Add about page

---
+++ pages/about.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>
+      This is a simple web applicadion made by NTI Gymnasiet Odenplans High
+      School Engineer's to document their class trip to Kagōshima Prefecture and
+      Tokyo City, Japan in January of 2023.
+    </p>
+    <p>
+      これは、NTI Gymnasiet Stockholm Odenplan の T4-22
+      クラスの鹿児島県と東京市への 2023 年 1
+      月の旅行のブログ投稿を表示および管理するために作成されたシンプルな Web
+      アプリケーションです。
+    </p>
+    <p>
+      Detta är en enklare webbapplikation gjord för av NTI Gymnasiet Stockholm
+      Odenplans TE-4 klass för att dokumentera sin klassresa til
+      Kagōshima-prefektur och Tokyo i januari 2023.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 80%;
+  margin: 0 auto;
+}
+p {
+  min-width: 20rem;
+  max-width: 30rem;
+  background-color: wheat;
+  margin: 1rem;
+  border: solid black;
+  padding: 1rem;
+  border-radius: 20px;
+  text-align: center;
+  font-size: 1.7rem;
+}
+</style>
